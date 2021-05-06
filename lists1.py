81f42e097b33039c0a0c55e2fad5b671aa8e29ba
Immanuel Alexander <i.alexander2012@yahoo.com>
2021-05-06 14:26:40 -0500

    adds list files

---
+++ lists1.py
@@ -0,0 +1,14 @@
+goats = []
+
+goats.append("lebron")
+goats.append("steph")
+goats.append("kobe")
+goats.append("julius")
+goats.append("MJ")
+
+index = 0
+
+for player in goats:
+    print(player)
+    if player == "MJ":
+        break
---
+++ lists2.py
@@ -0,0 +1,46 @@
+num  = [28, -86, 467, -984, 245, -5893, 248, -967]
+x = 34
+newls =[]
+
+# for number in num:
+#     print(number)
+#     total += number
+#     print(total)
+
+# for number in num:
+#    if number > huge:
+#        huge = number 
+# print(huge)
+
+# for number in num:
+#     if number % 2 == 0:
+#         print(number)
+
+# for number in num:
+#     if number >= 0:
+#         print(number)
+
+# for number in num:
+#     if number >= 0:
+#           newls.append(number)    
+# print(newls)
+
+for number in num:
+    newls.append(number * x)
+print(newls)
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
